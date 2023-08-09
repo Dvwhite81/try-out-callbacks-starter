87420d9cc9dbdb7457cd8defa7fdb37330254b2f
Dvwhite81 <123031200+Dvwhite81@users.noreply.github.com>
2023-08-08 23:50:32 -0400

    Add files via upload

---
+++ problems/13-min-value-callback.js
@@ -0,0 +1,37 @@
+/*******************************************************************************
+Write a function `minValueCallback` that accepts an array and an optional callback as arguments.
+If a callback is not passed in, then the function should return the smallest
+value in the array. If a callback is passed in, then the function should return
+the result of the smallest value being passed into the given callback.
+
+Examples:
+console.log(minValueCallback([64, 25, 49, 9, 100]));             // 9
+console.log(minValueCallback([64, 25, 49, 9, 100], Math.sqrt));  // 3
+
+*******************************************************************************/
+
+let minValueCallback = function(array, callback) {
+    let smallest = array[0];
+
+    for (let i = 0; i < array.length; i++) {
+        if (callback === undefined) {
+            if (array[i] < smallest) {
+                smallest = array[i];
+            }
+        }
+        else {
+            if (callback(array[i]) < smallest) {
+                smallest = callback(array[i]);
+            }
+        }
+    }
+    return smallest;
+};
+
+
+
+
+
+
+/*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
+module.exports = minValueCallback;
